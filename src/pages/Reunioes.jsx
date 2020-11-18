dda9ea7d25475b8a7cb607628b14820016f4edd3
EasyPeasy <BrayanWilis@outlook.com>
2020-11-17 21:06:27 -0300

    adicionei e modifiquei o estilo da pagina reunioes

--- src/pages/Reunioes.jsx
+++ src/pages/Reunioes.jsx
@@ -4,6 +4,7 @@ import AbaPrincipal from '../components/AbaPrincipal.jsx';
 import FotoSindico from '../assets/foto sindico.jpg';
 import IconeMicrofone from '../assets/icons/baseline_mic_black_18dp.png';
 import IconeCamera from '../assets/icons/baseline_videocam_black_18dp.png';
+import Postar from '../assets/icons/round_check_circle_outline_black_18dp.png';
 import Chat from '../components/Chat';
 
 function Reunioes(){
@@ -22,22 +23,28 @@ function Reunioes(){
                 </div>
                 <div className="painel-botoes">
                     <button className="microfone">
-                        <img className="icone-mic" src={IconeMicrofone} alt="icone do botao de microfone"/>
+                        <img src={IconeMicrofone} alt="icone do botao de microfone"/>
                     </button>
                     <button className="camera">
-                        <img className="icone-camera" src={IconeCamera} alt="icone do botao de tela"/>
+                        <img src={IconeCamera} alt="icone do botao de tela"/>
                     </button>
                 </div>
             </div>
-            <div className="coluna2">
+            <div className="coluna2r">
                 <div className="caixa-titulo">
                     <label className="titulo-reuniao"> Reunião Nº 35 dos sindícos </label>
                 </div>
                 <div className="caixa-chat">
                     <Chat pessoa="Vassi" texto="Olá pessoal, aqui é a Vassi" />
                     <Chat pessoa="Mariana" texto="Olá Vassi !!!" />
+                    <Chat pessoa="Helena" texto="Olá, eu vi que hoje você ira por em pauta a sobre a regra do som alto á noite, não é mesmo ?" />
                 </div>
-                <input className="caixa-texto-input"/>
+                <div className="caixa-texto-input">
+                    <input type="text" className="input-texto" />
+                    <button className="submit" src={Postar} >
+                        <img src={Postar} alt="icone do botao de postar"/>
+                    </button>
+                </div>
             </div>
         </div>
 
