6ff1a38c542a89bf64aa668e9e8bea10d343d8dd
José Pedro <josetanarea@hotmail.com>
2020-11-23 20:54:49 -0300

    Tela de serviços

---
+++ Tela de serviços/src/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/Primeiro'
+
+
+
+
+
+ReactDOM.render(
+  <Primeiro></Primeiro>,
+  document.getElementById('root')
+
+
+
+)
