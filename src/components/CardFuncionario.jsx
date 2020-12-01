1d39c56d9956097b54b66454349586b393d4d269
EasyPeasy <BrayanWilis@outlook.com>
2020-11-30 22:12:55 -0300

    fiz a tela estatica do funcionario

--- src/components/CardFuncionario.jsx
+++ src/components/CardFuncionario.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import '../styles/cardfuncionariosstyle.css';
+import Atualizar from '../assets/icons/baseline_refresh_black_18dp.png';
+import Escalas from '../assets/icons/baseline_hourglass_full_black_18dp.png';
+import Deletar from '../assets/icons/baseline_delete_forever_black_18dp.png';
 
 function CardFuncionario(props){
 
     return(
         <div className="cardfuncionario">
-            <img className="funcionario-fundo" alt="foto de fundo" />
-            <img className="funcionario-foto" alt="foto de fundo" />
+            <img className="funcionario-fundo" src={props.fotofundo} alt="foto de fundo" />
+            <img className="funcionario-foto" src={props.fotofuncionario} alt="foto de fundo" />
             <div className="caixa-informacoes">
                 <label className="funcionario-nome"> {props.nome} </label>
                 <label className="funcionario-cargo"> {props.cargo} </label>
@@ -13,9 +17,15 @@ function CardFuncionario(props){
                 <label className="funcionario-salario"> R$ {props.salario} </label>
             </div>
             <div className="caixa-acoes">
-                <button className="atualizar"></button>
-                <button className="escalas"></button>
-                <button className="deletar"></button>
+                <button className="atualizar">
+                    <img className="icone-botao-acao" src={Atualizar} alt="botao atualizar" />
+                </button>
+                <button className="escalas">
+                    <img className="icone-botao-acao" src={Escalas} alt="botao escalas" />
+                </button>
+                <button className="deletar">
+                    <img className="icone-botao-acao" src={Deletar} alt="botao deletar" />
+                </button>
             </div>
         </div>
     );
--- src/pages/Funcionarios.jsx
+++ src/pages/Funcionarios.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../styles/funcionariosstyle.css';
 import SideBar from '../components/SideBar';
+import CardFuncionario from '../components/CardFuncionario';
+import FundoFuncionario from '../assets/imagem de fundo funcionario.jpg';
+import FotoExemploFuncionario from '../assets/foto funcionario exemplo.jpg';
+import FotoExemploFuncionario2 from '../assets/foto funcionario exemplo 2.jpg';
+import FotoExemploFuncionario3 from '../assets/foto funcionario exemplo 3.jpg';
+import FotoExemploFuncionario4 from '../assets/foto funcionario exemplo 4.jpg';
+import FotoExemploFuncionario5 from '../assets/foto funcionario exemplo 5.jpg';
+import FotoExemploFuncionario6 from '../assets/foto funcionario exemplo 6.jpg';
 
 function Funcionarios(){
 
@@ -12,7 +20,13 @@ function Funcionarios(){
             <div className="conteudo-funcionarios">
                 <h1 className="titulo"> Equipe de Funcionarios </h1>
                 <div className="secao-cards">
-
+                    <CardFuncionario fotofundo={FundoFuncionario} fotofuncionario={FotoExemploFuncionario} nome="Samantha" cargo="Sindica" escala="Diurno" salario="3200"/>
+                    <CardFuncionario fotofundo={FundoFuncionario} fotofuncionario={FotoExemploFuncionario2} nome="Sergio Cabral" cargo="Contabilidade" escala="Estagiario" salario="3000"/>
+                    <CardFuncionario fotofundo={FundoFuncionario} fotofuncionario={FotoExemploFuncionario3} nome="Maria Helena" cargo="Gerente" escala="Noturno" salario="3500"/>
+                    <CardFuncionario fotofundo={FundoFuncionario} fotofuncionario={FotoExemploFuncionario4} nome="Otavio Sandiego" cargo="Socio" escala="Diurno" salario="7000"/>
+                    <CardFuncionario fotofundo={FundoFuncionario} fotofuncionario={FotoExemploFuncionario5} nome="Felipe Chaves" cargo="Arquiteto" escala="Diurno" salario="3000"/>
+                    <CardFuncionario fotofundo={FundoFuncionario} fotofuncionario={FotoExemploFuncionario6} nome="Suzana Pires" cargo="Porteira" escala="Noturno" salario="3500"/>
+                    
                 </div>
             </div>
         </div>
