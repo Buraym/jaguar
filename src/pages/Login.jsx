6f7d7e1a05601a2df9e47ba391147e4f78c9c2cb
EasyPeasy <BrayanWilis@outlook.com>
2020-12-04 16:06:24 -0300

    reformulei e otimizei a pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/loginstyle.css';
-import CampoLoginSenha from '../components/CampoLoginSenha';
+import FotoLogo from '../assets/logo exemplo 4.png';
+import { Link } from "react-router-dom";
+import ImagemBotao from '../assets/icons/outline_check_box_outline_blank_black_18dp.png';
 
 function Login() {
 
@@ -10,11 +12,34 @@ function Login() {
 
             <div className="caixalogo">
 
-                <label className="logo"> Jaguar </label>
+                <img src={FotoLogo} alt="foto logo" />
 
             </div>
 
-            <CampoLoginSenha/>
+            <div>
+                <form>
+                    <div className="Caixa">
+                        <label className="tag"> Login: </label>
+                        <input type="text" className="caixa_tag_input"/>
+                    </div>
+                    <div className="Caixa">
+                        <label className="tag"> Senha: </label>
+                        <input type="text" className="caixa_tag_input"/>
+                    </div>
+                </form>
+                <div className="Caixa_lembrar">
+                    <img className="caixa_selecao" src={ImagemBotao} alt="Caixa_de_seleção" ></img>
+                    <label className="lembrar"> Esqueceu sua senha ? Não tem cadastro </label>
+                </div>
+                <div className="Caixa_botao">
+                    <Link to="/home">
+                        <button className="botao_entrar">
+                            <label className="entrar-label"> Entrar </label>
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            {/*<CampoLoginSenha/>*/}
             
         </div>  
       
