d8133c4d2e72f34b7c5558fe8ab6417b4ca33b9e
EasyPeasy <BrayanWilis@outlook.com>
2020-12-07 10:42:57 -0300

    ajustes na pagina inicial do morador

--- src/components/Notificacao.jsx
+++ src/components/Notificacao.jsx
@@ -2,31 +2,23 @@ import React from 'react';
 import '../styles/notificacaostyle.css';
 
 function Notificacao(props) {
-
-    const valortitulo = document.getElementsByClassName("input-titulo").value;
-    const valortexto = document.getElementsByClassName("input-descricao").value;
-
     var data = new Date();
     var dia = data.getDate();
     var mes = data.getMonth();
     var ano = data.getFullYear();
     var data_atual = dia + "/" + (mes + 1) + "/" + ano;
 
-    function HandleEnviar(){
-        if (valortitulo && valortexto){
-
-        } else {
-            alert("Você não preencheu todos os campos");
-        }
-    }
-
     return(
-        <div className="caixa-notificacao" onSubmit={HandleEnviar}>
-            <form>
-                <input type="text" className="input-titulo"/>
-                <input type="text" className="input-descricao"/>
-                <input type="submit" className="envio" value={valortitulo, valortexto} />
-            </form>
+        <div className="caixa-notificacao">
+            <div>
+                <div className="caixa-notificacao-topico">
+                    <label> { props.titulo } </label>
+                    <label> { data_atual } </label>
+                </div>
+                <div className="caixa-notificacao-topico">
+                    <p className="input-descricao"> { props.descricao}</p>
+                </div>
+            </div>
         </div>
     );
 }
